0092546dd49bafd404ad967c29e6f5135e683db3
Katie Cannon <39453428+KatieCannon@users.noreply.github.com>
2020-08-06 16:32:28 +0100

    navbar setup

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from '../NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <div className='navBar'>
+            <h2>BookShelf</h2>
+            <h2>Challenge</h2>
+            <h1>ReadMore</h1>
+            <h2>BookList</h2>
+            <h2>SignUp</h2>
+        </div>
+    )
+}
+
+export default NavBar;
