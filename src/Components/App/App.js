d24f8a612d0505ceb5b1e43dd8a55a742c64ff1e
Katie Cannon <39453428+KatieCannon@users.noreply.github.com>
2020-08-06 17:25:47 +0100

    starting routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import NavBar from '../NavBar/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   const user = useState('kate');
@@ -8,6 +9,19 @@ function App() {
   return (
     <div className="App">
       <NavBar user={user[0]} />
+      <Router >
+        <Switch>
+          <Route path='/login'>
+
+          </Route>
+          <Route path='/signup'>
+
+          </Route>
+          <Route path='/challenge'>
+
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
